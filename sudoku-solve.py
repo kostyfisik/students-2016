c4e81039702ba3a5c4d797f5d2a10a80b7008659
DobriyLobster <dmitriy_93@mail.ru>
2016-03-24 14:15:23 +0200

    Update sudoku-solve.py

--- sudoku-solve.py
+++ sudoku-solve.py
@@ -16,6 +16,7 @@ for numbers in joined:
     if j>8:
         j=0
         i+=1
+print('Sudoku to solve:')
 print(sudoku)
 
 def get_options_temp(array):
@@ -30,16 +31,6 @@ def get_options_temp(array):
     print(a)
     print(options)
     
-#g=options[0][0]
-#print(g)
-#for j in range(9):
-#    if not array[1,j]==0:
-#        c=array[1,k]
-#        print(c)
-#        for k in range(0,8):
-#            if not options[k]==0:
-#                options[1+j][c]=0
-
 def is_in_array(array, char):
     if char in array:
         return True
@@ -55,8 +46,8 @@ def get_column(array, index):
 def get_cell(array, row, column):
     temp = np.zeros(9, dtype=np.int8)
     count=0
-    for i in range(row-1, row+2):
-        for j in range(column-1, column+2):
+    for i in range(((row-1)//3)*3, ((row-1)//3)*3+3):
+        for j in range(((column-1)//3)*3, ((column-1)//3)*3+3):
             temp[count]=array[i,j]
             count+=1
     return temp
@@ -86,7 +77,9 @@ def simple_solution(array):
 def get_options(array, row, column):
     temp=[]
     for value in range(1,9):
-        if not is_in_array(get_row(array,row), value) and not is_in_array(get_column(array, column), value) and not is_in_array(get_cell(array, row, column), value):
+        if (not is_in_array(get_row(array, row), value) 
+            and not is_in_array(get_column(array, column), value) 
+            and not is_in_array(get_cell(array, row, column), value)):
             temp.append(value)
     answer=np.array((temp), dtype=np.int8)
     return answer
@@ -98,23 +91,40 @@ def simple_solution(array):
         list_of_options=[]
         for i in range(9):
             for j in range(9):
+                if not array[i,j]==0:
+                    continue
                 list_of_options.append(get_options(array, i, j))
-        count1=0
+        count=0
         for i in range(9):
             for j in range(9):
-                if len(list_of_options[count])==1 and array[i,j]==0:
-                    array[i,j] = join(list_of_options[count1])
+                if len(list_of_options[count])==1:
+                    array[i,j] = int(list_of_options[count])
+                    #print('row = ', i, ', column = ', j, ', value = ', array[i,j])
                     changed=True
-                count1+=1
+                    break
+                count+=1
     return array
 
-#print(get_row(sudoku,4))
-#print(get_column(sudoku,1))
-print(get_cell(sudoku,2,2))
-if is_in_array(get_row(sudoku,1),4):
-    print('yes')
-else:
-    print('no')
-print(get_options(sudoku, 1, 1))
+def is_solved(array):
+    if 0 in array:
+        return False
+    else:
+        return True
+
+#print(get_row(sudoku,3))
+#print(get_column(sudoku,5))
+#print(get_cell(sudoku,3,5))
+#if is_in_array(get_row(sudoku,1),4):
+#    print('yes')
+#else:
+#    print('no')
+#print(get_options(sudoku, 3, 5))
 #print(simple_solution(sudoku))
-print(6//3+1)
+#print(6//3+1)
+#simple_solution(sudoku)
+#if is_solved(sudoku):
+#    print('Sudoku is solved. Ansewer: ')
+#    print(sudoku)
+#else:
+#    print('Sudoku is not solved. Last statement:')
+#    print(sudoku)
